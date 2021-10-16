beae8d0834dca72259d4781696db62ba356f523d
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2021-10-16 10:02:58 +0000

    Add Sound tile to Quick Settings
        abc ezio84: adapt to O, show volume panel with long press,
        use full dnd (no interruptions) as silent mode
    
        @beanstown106: Sound Tile improvements
        *added longpress action
        *added more descriptive labels for each state instead of always saying sound
    
        ezio84: Adapt for N
        Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96
    
    Squashed commit of the following:
    
    commit 69f86d9a38b5a942e3f9c914895ee45c0079e8e9
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Apr 13 03:32:51 2018 +0200
    
        base: SystemUI: stop crashing qs tiles during boot
    
        this triggers my ocd
        continues a579c2430f1a0bf088023b4283dcec37776c070d
    
        Change-Id: I31719b595cb50a330f26e25074243e04c06f63c1
        Signed-off-by: xyyx <xyyx@mail.ru>
    
    commit f419619c033fa99a809f53aa82dbdd7d6732177c
    Author: xyyx <xyyx@mail.ru>
    Date:   Thu Sep 28 11:55:36 2017 +0800
    
        SoundTile: Change ZEN_MODE_NO_INTERRUPTIONS to ZEN_MODE_ALARMS
    
        Change-Id: I5edaaa4d551630049b29e09d3cecfb5ffd503e99
    
    commit 916645271f57c47d60bb0a04c4e51776b51a18f1
    Author: Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
    Date:   Sun Sep 18 22:33:08 2016 +0200
    
        Add Sound tile to Quick Settings
    
        abc ezio84: adapt to O, show volume panel with long press,
        use full dnd (no interruptions) as silent mode
    
        @beanstown106: Sound Tile improvements
        *added longpress action
        *added more descriptive labels for each state instead of always saying sound
    
        ezio84: Adapt for N
        Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96
    
    Change-Id: I178bfa69ff181f0f65f94ffae1444aaea5f21a80
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -53,6 +53,7 @@ import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SoundSearchTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -104,6 +105,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<AODTile> mAODTileProvider;
     private final Provider<SoundSearchTile> mSoundSearchTileProvider;
+    private final Provider<SoundTile> mSoundTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -145,7 +147,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<AmbientDisplayTile> ambientDisplayTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
             Provider<AODTile> aodTileProvider,
-            Provider<SoundSearchTile> soundSearchTileProvider) {
+            Provider<SoundSearchTile> soundSearchTileProvider,
+            Provider<SoundTile> soundTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -183,6 +186,7 @@ public class QSFactoryImpl implements QSFactory {
         mCaffeineTileProvider = caffeineTileProvider;
         mAODTileProvider = aodTileProvider;
         mSoundSearchTileProvider = soundSearchTileProvider;
+        mSoundTileProvider = soundTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -263,6 +267,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mAODTileProvider.get();
             case "soundsearch":
                 return mSoundSearchTileProvider.get();
+            case "sound":
+                return mSoundTileProvider.get();
         }
 
         // Custom tiles
