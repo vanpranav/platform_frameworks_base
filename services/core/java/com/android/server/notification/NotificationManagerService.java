43aef16c9c1a39582ead57a30525ce4f1eab2b6c
wangmingming1 <wangmingming1@xiaomi.com>
2021-11-07 18:43:23 +0800

    Toast: fix layer leak
    Bug: 205340284
    Test: atest NotificationManagerServiceTest
    
    Change-Id: I48b6480f05d76909ab5c3de20bfbef726daacfb9
    Signed-off-by: wangmingming1 <wangmingming1@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -7732,6 +7732,8 @@ public class NotificationManagerService extends SystemService {
             int index = mToastQueue.indexOf(record);
             if (index >= 0) {
                 mToastQueue.remove(index);
+                mWindowManagerInternal.removeWindowToken(record.windowToken,
+                        true /* removeWindows */, record.displayId);
             }
             record = (mToastQueue.size() > 0) ? mToastQueue.get(0) : null;
         }
