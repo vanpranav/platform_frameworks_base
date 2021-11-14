5ca797448b2cfe151ff2270f33c5186b224d4c80
jhonboy121 <alfredmathew05@gmail.com>
2021-11-15 01:15:37 +0530

    base: allow toggling location privacy indicator[1/2]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10719,6 +10719,13 @@ public final class Settings {
         public static final String GMS_ENABLED = "gms_enabled";
 
         /**
+         * Whether to show privacy indicator for location
+         * Default value is based on it's device config flag
+         * @hide
+         */
+        public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+        
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
