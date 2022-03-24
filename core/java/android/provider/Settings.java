6f9289aec1fd420872b9beef3c4f163d5ed26347
beanstown106 <nbenis106@gmail.com>
2022-03-24 20:35:42 +0800

    base: incall vibration options [1/3]
     *allow setting vibration when call is connected
     *allow setting vibration when call is disconnected
     *allow setting vibration for call waiting
     *this works with google and aosp dialer :)
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I14a403189f37e45fa4b999ab015bde8f89ae3da4
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5378,6 +5378,24 @@ public final class Settings {
          */
         public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
         
+	/**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+        /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+        /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+        
         /**
          * Arcane Idle Manager
          * @hide
