Evolution-X-AOSP/frameworks_base

[SQUASH] Custom logo customizations [1/2]
* Original placement logic and custom logo patches
  by LorDClockaN and varund7726 respectively.
* @neobuddy89: Complete rewrite for oreo with darkicon dispatcher.

-AICP'fied by @eyosen:
  * We are AICP after all.
  * Also make naming rom independent.
  * Brought up to Aicp Pie, 10, 11, 12
  * Includes 27dc548f8c483429af93a265492f2e0e7e514112
	fwb: statusbar logo code cleanup
  * and more code cleanup

Change-Id: I9f4a9331a3867712f69579f764ef2cebd0bbe717
Signed-off-by: Julian Veit <claymore1298@gmail.com>

packages/SystemUI/src/com/android/systemui/evolution/logo/LogoImageView.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.evolution.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageView extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageView(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition == 1 || mLogoPosition == 3){
+            return true;
+        }
+        return false;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/evolution/logo/LogoImageViewQuick.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.evolution.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageViewQuick extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageViewQuick(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageViewQuick(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageViewQuick(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition != 2){
+            return true;
+        }
+        return false;
+    }
+
+
+}

packages/SystemUI/src/com/android/systemui/evolution/logo/LogoImageViewQuickRight.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.evolution.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageViewQuickRight extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageViewQuickRight(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageViewQuickRight(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageViewQuickRight(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition != 3){
+            return true;
+        }
+        return false;
+    }
+
+
+}

packages/SystemUI/src/com/android/systemui/evolution/logo/LogoImageViewRight.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.evolution.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageViewRight extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageViewRight(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageViewRight(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageViewRight(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition == 0 || mLogoPosition == 2){
+            return true;
+        }
+        return false;
+    }
+
+}

vanpranav/platform_frameworks_base

Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

packages/SystemUI/src/com/android/systemui/arcana/logo/LogoImageView.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.arcana.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageView extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageView(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition == 1 || mLogoPosition == 3){
+            return true;
+        }
+        return false;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/arcana/logo/LogoImageViewQuick.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.arcana.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageViewQuick extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageViewQuick(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageViewQuick(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageViewQuick(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition != 2){
+            return true;
+        }
+        return false;
+    }
+
+
+}

packages/SystemUI/src/com/android/systemui/arcana/logo/LogoImageViewQuickRight.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.arcana.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageViewQuickRight extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageViewQuickRight(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageViewQuickRight(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageViewQuickRight(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition != 3){
+            return true;
+        }
+        return false;
+    }
+
+
+}

packages/SystemUI/src/com/android/systemui/arcana/logo/LogoImageViewRight.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.arcana.logo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class LogoImageViewRight extends LogoImage
+{
+
+    private Context mContext;
+
+    public LogoImageViewRight(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageViewRight(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageViewRight(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    @Override
+    protected boolean isLogoHidden()
+    {
+        if(mLogoPosition == 0 || mLogoPosition == 2){
+            return true;
+        }
+        return false;
+    }
+
+}